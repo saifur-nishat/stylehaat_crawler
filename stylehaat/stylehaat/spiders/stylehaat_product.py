a3e795d33fb866230ee9c1d6a448002447acc6d8
saifur-nishat <saifur.nishat@gmail.com>
2020-02-19 14:44:05 +0600

    modified spider

--- stylehaat/stylehaat/spiders/stylehaat_product.py
+++ stylehaat/stylehaat/spiders/stylehaat_product.py
@@ -12,13 +12,14 @@ class StylehaatProductSpider(CrawlSpider):
     start_urls = ['http://www.stylehaat.com/']
 
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(restrict_xpaths='//ul[contains(@class,"st_mega_menu")]/li/a[contains(@class,"is_parent")]'), follow=True),
+        Rule(LinkExtractor(restrict_xpaths='//a[contains(@class,"product-name")]'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(restrict_xpaths='//li[@id="pagination_next_bottom"]/a[@href]'), follow=True),
     )
 
     def parse_item(self, response):
         item = ItemLoader(item=StylehaatItem(), response=response)
-        item.add_xpath('breadcrumb',
-                       '//section[@class="breadcrumb"]/ul/li[position()>1 and contains(@typeof,"v:Breadcrumb")]/a/text()')
+        item.add_xpath('breadcrumb', '//section[@class="breadcrumb"]/ul/li[position()>1 and contains(@typeof,"v:Breadcrumb")]/a/text()')
         item.add_xpath('product_name', '//section[@class="breadcrumb"]/ul/li[last()]/span/text()')
         item.add_xpath('product_old_price', '//span[@id="old_price_display"]/text()')
         item.add_xpath('product_new_price', '//span[@id="our_price_display"]/text()')
