d8583f4f68c11f04f268857da95b2da8222558e0
saifur-nishat <saifur.nishat@gmail.com>
2020-02-19 14:34:16 +0600

    Created spider named stylehaat_product

--- stylehaat/stylehaat/spiders/stylehaat_product.py
+++ stylehaat/stylehaat/spiders/stylehaat_product.py
@@ -2,6 +2,8 @@
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
+from scrapy.loader import ItemLoader
+from ..items import StylehaatItem
 
 
 class StylehaatProductSpider(CrawlSpider):
@@ -14,8 +16,13 @@ class StylehaatProductSpider(CrawlSpider):
     )
 
     def parse_item(self, response):
-        item = {}
-        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
-        #item['name'] = response.xpath('//div[@id="name"]').get()
-        #item['description'] = response.xpath('//div[@id="description"]').get()
-        return item
+        item = ItemLoader(item=StylehaatItem(), response=response)
+        item.add_xpath('breadcrumb',
+                       '//section[@class="breadcrumb"]/ul/li[position()>1 and contains(@typeof,"v:Breadcrumb")]/a/text()')
+        item.add_xpath('product_name', '//section[@class="breadcrumb"]/ul/li[last()]/span/text()')
+        item.add_xpath('product_old_price', '//span[@id="old_price_display"]/text()')
+        item.add_xpath('product_new_price', '//span[@id="our_price_display"]/text()')
+        img_link = response.xpath('//div[@id="view_full_size"]//div[@class="item"]//img/@src').extract()
+        item.add_value('image_urls', img_link)
+        item.add_xpath('more_info', '//div[@id="more_info_block"]//div[@class="pa_content"]//p/text()')
+        yield item.load_item()
