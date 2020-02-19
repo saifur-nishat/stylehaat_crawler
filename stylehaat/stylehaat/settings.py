8a60b1d6a69b4992d345266ff44f8b8780d2702d
saifur-nishat <saifur.nishat@gmail.com>
2020-02-19 13:05:14 +0600

    Modified settings for images download. Changed ITEM_PIPELINES, IMAGES_STORE, DOWNLOAD_TIMEOUT

--- stylehaat/stylehaat/settings.py
+++ stylehaat/stylehaat/settings.py
@@ -64,9 +64,13 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'stylehaat.pipelines.StylehaatPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'scrapy.pipelines.images.ImagesPipeline': 1,
+   # 'crawler.pipelines.CrawlerPipeline': 300,
+}
+
+IMAGES_STORE = 'images'
+DOWNLOAD_TIMEOUT = 1200
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
